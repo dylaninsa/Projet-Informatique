7aded56054f4639e1949bb3ba1c437c393c38537
dylaninsa <dylan.insa@insa-rouen.fr>
2023-10-04 10:34:36 +0200

    Mise en place du menu

--- main.pas
+++ main.pas
@@ -4,11 +4,20 @@ program Cluedo;
 uses unite;
 
 
-//procedure
+procedure menu(c : Integer);
 
+begin
+    writeln('Que voulez-vous faire :');
+    writeln('   1 : Lancer une partie');
+    writeln('   2 : Afficher les règles du jeu');
+    writeln('   3 : Quitter');
+    repeat
+        readln(c);
+    until ((c >= 1) AND (c <= 3));
+end;
 
 var c : Integer;
 
 begin
-    
+    menu(c);
 end.
