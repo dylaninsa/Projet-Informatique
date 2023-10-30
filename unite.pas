d1043ab111c4fc805ddff2bcf2b36ea7e2ccbdb8
ninonmorin <ninon.morin@hotmail.com>
2023-10-30 18:28:41 +0100

    Ajout Cartes INSA

--- unite.pas
+++ unite.pas
@@ -7,7 +7,7 @@ Const MAX = 50;
 
 Type Enviro = (Manoir, INSA);
 
-Type ListeCartes = (Colonel_Moutarde, Docteur_Olive, Madame_Pervenche, Mademoiselle_Rose, Madame_Leblanc, Poignard, Chandelier, Revolver, Corde, Matraque, Clef_Anglaise, Cuisine, Grand_Salon, Petit_Salon, Salle_a_manger, Bureau, Bibliotheque, Veranda, Studio, Hall);
+Type ListeCartes = (Colonel_Moutarde, Docteur_Olive, Madame_Pervenche, Mademoiselle_Rose, Madame_Leblanc, Poignard, Chandelier, Revolver, Corde, Matraque, Clef_Anglaise, Cuisine, Grand_Salon, Petit_Salon, Salle_a_manger, Bureau, Bibliotheque, Veranda, Studio, Hall, Monsieur_Bredel, Madame_LArcheveque, Le_Directeur, Yohann_Lepailleur, Infirmiere, Seringue, Moteur_4_temps, Alteres, Flacon_Acide, Ordi, Pouf_Rouge, Cafete, Amphi_Tillon, Infirmerie, Vestiaires, Labo, DUBRJ11, Bureau_du_directeur, Toilettes, BU);
 
 Type Coords = Array [1..2] of Integer;
 
