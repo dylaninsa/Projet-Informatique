a3f16ff7aca5db040f03547b11676f477cbb2f3e
ninonmorin <ninon.morin@hotmail.com>
2023-11-01 14:26:30 +0100

    ajout signature (vérifier hypo)

--- partie.pas
+++ partie.pas
@@ -7,7 +7,15 @@ uses unite, Crt, configurationPartie;
 
 
 procedure jeu(etui:Array of ListeCartes; plat:Plateau; var joueurs:ListeJoueurs);
-
+//procedure tour(plat:Plateau; etui:Array of ListeCartes; var joueurs:ListeJoueurs; accusation:Boolean);
+//procedure finPartie(joueurs:ListeJoueurs; accusation:Boolean);
+//procedure quitterSauvegarder(joueurs:ListeJoueurs; plat:Plateau; etui:Array of ListeCartes; var sauvegarde:File);
+//procedure deplacement(plat:Plateau; lancer:Integer; var joueurs:ListeJoueurs);
+//procedure lancerDes(lancer:Integer);
+//procedure afficherCartes(joueurs:ListeJoueurs);
+//procedure faireHypothese(joueurs:ListeJoueurs; hypo:ListeCartes);
+//procedure demandeJoueur(hypo:ListeCartes;joueurs:ListeJoueurs);
+//procedure accusation(etui:Array of ListeCartes; var joueurs:ListeJoueurs; accusation:Boolean);
 
 Implementation
 
@@ -22,6 +30,15 @@ begin
 
     
 end;
+//procedure tour(plat:Plateau; etui:Array of ListeCartes; var joueurs:ListeJoueurs; accusation:Boolean);
+//procedure finPartie(joueurs:ListeJoueurs; accusation:Boolean);
+//procedure quitterSauvegarder(joueurs:ListeJoueurs; plat:Plateau; etui:Array of ListeCartes; var sauvegarde:File);
+//procedure deplacement(plat:Plateau; lancer:Integer; var joueurs:ListeJoueurs);
+//procedure lancerDes(lancer:Integer);
+//procedure afficherCartes(joueurs:ListeJoueurs);
+//procedure faireHypothese(joueurs:ListeJoueurs; hypo:ListeCartes);
+//procedure demandeJoueur(hypo:ListeCartes;joueurs:ListeJoueurs);
+//procedure accusation(etui:Array of ListeCartes; var joueurs:ListeJoueurs; accusation:Boolean);
 
 
 end.
